e8482a8285f6c0abb59b74b951770bf4e44c7cd0
GAUMUC <gaumucvt@gmail.com>
2024-11-04 21:24:51 +0700

    step two

---
+++ scripts.js
@@ -0,0 +1,27 @@
+let links = [
+  "https://www.youtube.com/watch?v=RvNcGgMtf38",
+  "https://www.facebook.com/plugins/video.php?href=https://web.facebook.com/reel/1185408302521740&width=400",
+  "https://www.youtube.com/watch?v=1QXBNUNS3_I",
+  "https://www.youtube.com/watch?v=1QXBNUNS3_I",
+  "https://www.youtube.com/watch?v=1QXBNUNS3_I",
+  "https://www.youtube.com/watch?v=1QXBNUNS3_I",
+  "https://www.youtube.com/watch?v=1QXBNUNS3_I",
+  "https://www.youtube.com/watch?v=1QXBNUNS3_I",
+  "https://www.youtube.com/watch?v=1QXBNUNS3_I",
+];
+
+links = links.map((link) => {
+  if (link.includes("youtube.com/watch?v=")) {
+    return link.replace("youtube.com/watch?v=", "youtube.com/embed/");
+  }
+  return link;
+});
+
+links.forEach((link) => {
+  const iframe = document.createElement("iframe");
+  iframe.src = link;
+  iframe.allow =
+    "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture";
+  iframe.allowFullscreen = true;
+  document.body.appendChild(iframe);
+});
