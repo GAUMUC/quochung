2dcd2d6c11ef0a2f9abfcac8483c1be2d347e5ef
GAUMUC <gaumucvt@gmail.com>
2024-11-04 21:39:51 +0700

    Update scripts.js
    Edf

--- scripts.js
+++ scripts.js
@@ -2,6 +2,7 @@ let links = [
   "https://www.youtube.com/watch?v=RvNcGgMtf38",
   "https://www.facebook.com/plugins/video.php?href=https://web.facebook.com/reel/1185408302521740&width=400",
   "https://www.youtube.com/watch?v=1QXBNUNS3_I",
+  "https://www.facebook.com/share/r/DxsT9FAnxbxNMHUF/",
 ];
 
 links = links.map((link) => {
