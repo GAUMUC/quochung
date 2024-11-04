59d6a4b8de38213b97d29d616727508d829e1b16
GAUMUC <gaumucvt@gmail.com>
2024-11-05 06:57:25 +0700

    ads

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 let links = [
   "https://www.youtube.com/watch?v=RvNcGgMtf38",
-  "https://www.facebook.com/plugins/video.php?href=https://web.facebook.com/reel/1185408302521740&width=400",
+  "https://www.facebook.com/plugins/video.php?href=https://web.facebook.com/reel/1185408302521740",
+  "https://www.facebook.com/plugins/video.php?href=https://web.facebook.com/reel/1097223367951825",
   "https://www.youtube.com/watch?v=1QXBNUNS3_I",
   "https://www.youtube.com/watch?v=V6e34SBzyik?si=gCHQCTtCKFXaFMCh",
   "https://www.youtube.com/watch?v=V6e34SBzyik",
@@ -8,7 +9,7 @@ let links = [
 
 links = links.map((link) => {
   if (link.includes("youtube.com/watch?v=")) {
-    return link.replace("youtube.com/watch?v=", "youtube.com/embed/", "");
+    return link.replace("youtube.com/watch?v=", "youtube.com/embed/");
   }
   return link;
 });
