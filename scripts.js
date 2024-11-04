339ef51ee2b7dc5f9f56bd6b8b2252fadb295541
GAUMUC <gaumucvt@gmail.com>
2024-11-04 22:45:32 +0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,7 +1,6 @@
 let links = [
   "https://www.youtube.com/watch?v=RvNcGgMtf38",
   "https://www.facebook.com/plugins/video.php?href=https://web.facebook.com/reel/1185408302521740&width=400",
-  "https://www.facebook.com/reel/8339511266117672",
   "https://www.youtube.com/watch?v=1QXBNUNS3_I",
   "https://www.youtube.com/watch?v=V6e34SBzyik?si=gCHQCTtCKFXaFMCh",
   "https://www.youtube.com/watch?v=V6e34SBzyik",
@@ -10,10 +9,6 @@ let links = [
 links = links.map((link) => {
   if (link.includes("youtube.com/watch?v=")) {
     return link.replace("youtube.com/watch?v=", "youtube.com/embed/", "");
-    else if (link.includes("facebook.com/reel/")) {
-    // Mở trong tab mới nếu là Facebook Reel
-    window.open(link, "_blank");
-    return null;
   }
   return link;
 });
