1b9a6842f2cd07f31d7b7c6cad018f421967140d
GAUMUC <gaumucvt@gmail.com>
2024-11-05 08:28:01 +0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,5 @@
 let links = [
   "https://www.youtube.com/watch?v=RvNcGgMtf38",
-  "https://www.facebook.com/plugins/video.php?href=https://web.facebook.com/reel/1185408302521740",
   "https://www.facebook.com/plugins/video.php?href=https://web.facebook.com/reel/1185408302521740&width=400",
   "https://www.facebook.com/plugins/video.php?href=https://web.facebook.com/reel/1097223367951825",
   "https://www.youtube.com/watch?v=1QXBNUNS3_I",
